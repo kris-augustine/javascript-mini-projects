7080329acd2f29e920a6a893e743eb8848b6eef1
christian augustine <chrisaugustine900@gmail.com>
2022-06-18 18:47:33 -0700

    first project

---
+++ 01-Counter/app.js
@@ -0,0 +1,29 @@
+// Set initial count
+let count = 0
+
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList
+        if (styles.contains('decrease')) {
+            count--
+        } else if (styles.contains('increase')) {
+            count++
+        } else {
+            count = 0
+        }
+        // adding color base on increment or decrement
+        if (count > 0) {
+            value.style.color = 'green'
+        }
+        if (count < 0) {
+            value.style.color = 'red'
+        }
+        if (count === 0) {
+            value.style.color = '#000'
+        }
+        value.textContent = count
+    })
+})
